722ad37acfb96ce5e153c5e265cb8844cc75bf28
Artur112266 <61555792+Artur112266@users.noreply.github.com>
2020-02-29 20:57:04 +0400

    Git-Github

---
+++ lesson6-git-github.py
@@ -0,0 +1,8 @@
+# Create a new repository
+# Clone url (copy) cd (paste) 
+# git status (informacia)
+# git add "file" or file, or (git add -A (add all files))
+# git commit -m "comment"
+# git push
+# git pull (download)
+print("hello world")
